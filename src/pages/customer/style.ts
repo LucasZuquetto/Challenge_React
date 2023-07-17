3c111edc963fbc16060cf20346c526b02b4b6443
Lucas Zuquetto <lucasdeozuquetto@hotmail.com>
2023-07-17 14:31:12 -0300

    feat: button style done

--- src/pages/customer/style.ts
+++ src/pages/customer/style.ts
@@ -10,19 +10,20 @@ export const CustomerContent = styled.div`
    min-height: 200px;
    border-radius: 20px;
    padding: 20px 175px 20px 29px;
+
    select {
-         width: 20rem;
-         border: 1px solid #c6d5db;
-         background: #ffffff;
-         color: #205266;
-         font-weight: 400;
-         font-size: 13px;
-         padding: 5px 16px 5px 14px;
-         border-radius: 5px;
-      }
-      select:focus {
-         outline: none;
-      }
+      width: 20rem;
+      border: 1px solid #c6d5db;
+      background: #ffffff;
+      color: #205266;
+      font-weight: 400;
+      font-size: 13px;
+      padding: 5px 16px 5px 14px;
+      border-radius: 5px;
+   }
+   select:focus {
+      outline: none;
+   }
    span {
       color: #205266;
       font-size: 13px;
@@ -35,7 +36,7 @@ export const CustomerContent = styled.div`
       display: flex;
       justify-content: space-between;
       height: 32px;
-      
+
       button {
          cursor: pointer;
          width: 80px;
@@ -63,13 +64,59 @@ export const CustomerContent = styled.div`
          }
       }
    }
-   & > div:nth-child(5), & > div:nth-child(7), & > div:nth-child(9) {
+   & > div:nth-child(5),
+   & > div:nth-child(7),
+   & > div:nth-child(9) {
       display: flex;
       flex-direction: column;
       gap: 10px;
       div {
          gap: 12px;
          display: flex;
+      }
+   }
+   & > button {
+      cursor: pointer;
+      width: 140px;
+      font-weight: 700;
+      font-size: 13px;
+      color: #205266;
+      background-color: white;
+      border-radius: 5px;
+      height: 32px;
+      border: 1px solid #205266;
+   }
+   & > button:hover {
+      filter: brightness(95%);
+   }
+   & > div:last-child {
+      display: flex;
+      gap: 10px;
+      button:hover {
+         filter: brightness(95%);
+      }
+      button:first-child {
+         cursor: pointer;
+         width: 140px;
+         font-weight: 700;
+         font-size: 13px;
+         color: white;
+         background-color: #63d391;
+         border: none;
+         border-radius: 5px;
+         height: 32px;
+      }
+      button:last-child {
+         cursor: pointer;
+         width: 80px;
+         font-weight: 700;
+         font-size: 13px;
+         color: #205266;
+         background-color: white;
+         border: none;
+         border-radius: 5px;
+         height: 32px;
+         text-decoration: underline;
       }
    }
 `;
